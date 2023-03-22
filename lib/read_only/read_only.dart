881bf5c98b8258dd3c3fcd98ee21dba8200e2bb1
divyabarvaliya <54199646+divyabarvaliya@users.noreply.github.com>
2023-03-22 16:02:39 +0530

    finaly1 commit

--- lib/read_only/read_only.dart
+++ lib/read_only/read_only.dart
@@ -1,15 +1,16 @@
+
 ///--------------------->first method
 // void main() {
-
+//
 //   final list = [
 //     {'name': 'John', 'age': 30},
 //     {'name': 'Jane', 'age': 25},
 //     {'name': 'Bob', 'age': 40},
 //   ];
-
-
+//
+//
 //   final map=Map.fromIterable(list,key:(element) => element['name'],value: (element) => element['age']);
-
+//
 //   print(map); // {John: 30, Jane: 25, Bob: 40}
 // }
 
@@ -29,8 +30,70 @@
 // {
 //   final list = ['John', 'Jane', 'Bob'];
 
-//   var maps = list.asMap();
+//   var maps = list.asMap(); 
 //   print(maps); // {0: John, 1: Jane, 2: Bob}
 // }
 
-///--------------------->third method
+///--------------------->fourth method
+// class Person {
+//   final String name;
+//   final int age;
+
+//   Person({required this.name, required this.age});
+// }
+
+// void main() {
+//   List<Person> people = [
+//     Person(name: 'Alice', age: 25),
+//     Person(name: 'Bob', age: 30),
+//     Person(name: 'Charlie', age: 35),
+//   ];
+
+//   Map<String, int> peopleMap = Map.fromIterable(
+//     people,
+//     key: (person) => person.name,
+//     value: (person) => person.age,
+//   );
+
+//   //------>people[0].age//--> you can acsses like this
+
+//   print(peopleMap); // Output: {Alice: 25, Bob: 30, Charlie: 35}
+// }
+
+
+///------------------------->fifth method--{{}}
+// void main() {
+//   List<Map<String, dynamic>> myList = [
+//     {'id': 1, 'name': 'Alice'},
+//     {'id': 2, 'name': 'Bob'},
+//     {'id': 3, 'name': 'Charlie'},
+//   ];
+
+//   Map<int, String> myMap = Map.fromIterable(myList,
+//       key: (item) => item['id'], value: (item) => item['name']);
+
+//   print(myMap); // {1: Alice, 2: Bob, 3: Charlie}
+// }
+
+///------------------------->six method
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
